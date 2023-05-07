2d32de291364f7194b0e67e8a3ac1ac86a0806ec
Samana19 <samanashrestha02@gmail.com>
2023-05-07 18:38:04 +0545

    colors, font styles and spacing

---
+++ lib/core/text_style.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import './colors.dart';
+
+const TextStyle headline = TextStyle(
+  fontSize: 16,
+  color: grayText,
+  fontWeight: FontWeight.normal,
+);
+
+const TextStyle headline1 = TextStyle(
+  fontSize: 18,
+  color: whiteText,
+  fontWeight: FontWeight.w600,
+);
+const TextStyle headline3 = TextStyle(
+  fontSize: 14,
+  color: grayText,
+  fontWeight: FontWeight.bold,
+);
+const TextStyle hintStyle = TextStyle(
+  fontSize: 14,
+  color: grayText,
+  fontWeight: FontWeight.bold,
+);
