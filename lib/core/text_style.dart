025d0d3b58ebc65de4054e7bccbd005c1f4af359
Samana Shrestha <samanashrestha02@gmail.com>
2023-05-24 10:30:30 +0545

    Adding font family to text style

--- lib/core/text_style.dart
+++ lib/core/text_style.dart
@@ -1,29 +1,33 @@
 import 'package:flutter/material.dart';
 import './colors.dart';
 
-const TextStyle headline = TextStyle(
-  fontSize: 16,
-  color: grayText,
-  fontWeight: FontWeight.normal,
-);
-const TextStyle headlinebold = TextStyle(
-  fontSize: 38,
-  color: darkBlueText,
-  fontWeight: FontWeight.bold,
-);
+class CustomTextStyles {
+  static const String fontFamily = "Monsterrat";
 
-const TextStyle headline1 = TextStyle(
-  fontSize: 18,
-  color: whiteText,
-  fontWeight: FontWeight.w600,
-);
-const TextStyle headline3 = TextStyle(
-  fontSize: 14,
-  color: grayText,
-  fontWeight: FontWeight.bold,
-);
-const TextStyle hintStyle = TextStyle(
-  fontSize: 14,
-  color: grayText,
-  fontWeight: FontWeight.bold,
-);
+  static const TextStyle headline = TextStyle(
+    fontSize: 16,
+    color: grayText,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle headlinebold = TextStyle(
+    fontSize: 38,
+    color: darkBlueText,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle headline1 = TextStyle(
+    fontSize: 18,
+    color: whiteText,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle headline3 = TextStyle(
+    fontSize: 14,
+    color: grayText,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle hintStyle = TextStyle(
+    fontSize: 14,
+    color: grayText,
+    fontWeight: FontWeight.bold,
+  );
+}
