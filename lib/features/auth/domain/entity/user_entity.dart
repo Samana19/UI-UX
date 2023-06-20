6c6a3ceb1c0637c9e0592089a3740ae2346cb63b
Samana Shrestha <samanashrestha02@gmail.com>
2023-06-20 19:53:44 +0545

    user entity

---
+++ lib/features/auth/domain/entity/user_entity.dart
@@ -0,0 +1,11 @@
+class UserEntity {
+  final String email;
+  final String username;
+  final String password;
+
+  UserEntity({
+    required this.email,
+    required this.username,
+    required this.password,
+  });
+}
