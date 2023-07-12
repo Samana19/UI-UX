2434525940a78ded06cb1df91c9d846a63484de6
Samana Shrestha <samanashrestha02@gmail.com>
2023-07-12 22:22:49 +0545

    changes

--- lib/app.dart
+++ lib/app.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:the_daily_digest/features/auth/presentation/view/login_page.dart';
 import 'package:the_daily_digest/features/auth/presentation/view/signup_page.dart';
 import 'package:the_daily_digest/features/splash/presentation/view/splash_screen.dart';
-import 'core/common/widget/bottom_nav.dart';
-import 'views/dashboard_page.dart';
+import 'features/dashboard/presentation/view/bottom_nav.dart';
+import 'features/dashboard/presentation/view/bottom_view/dashboard/dashboard_page.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -19,11 +19,11 @@ class _MyAppState extends State<MyApp> {
         debugShowCheckedModeBanner: false,
         initialRoute: '/',
         routes: {
-          '/': (context) => const HomePage(),
+          '/': (context) => const BottomNavBar(),
           '/login': (context) => const LoginPage(),
           '/signup': (context) => const SignUpPage(),
           '/home': (context) => const DashboardPage(),
-          '/navbar': (context) => const BottomNavBar(),
+          '/bottomnav': (context) => const BottomNavBar(),
         });
   }
 }
